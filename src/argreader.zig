2bb642f492a6731175321623701c99c3fb685704
Liam O'Kane <ldokane01@gmail.com>
2024-08-04 13:03:41 +1000

    attempt short flags -abd

--- src/argreader.zig
+++ src/argreader.zig
@@ -15,14 +15,12 @@ const Command = struct {
         return Command{ .command = command, .flags = flags };
     }
 
-    fn deinit(self: Command) !void {
+    fn deinit(self: Command) void {
         self.flags.deinit();
     }
 };
 
 const Flag = struct { head: []const u8, tail: ?[]const u8 };
-
-pub fn main() !void {}
 
 pub fn readArgsIntoCommand(args: []const u8, allocator: std.mem.Allocator) !Command {
     var command = Command.init(allocator);
@@ -32,11 +30,20 @@ pub fn readArgsIntoCommand(args: []const u8, allocator: std.mem.Allocator) !Comm
 
     var prevFlag: ?Flag = null;
 
+    // grab shortflags,
+    var shortFlags = std.ArrayList([]u8).init(allocator);
+    defer shortFlags.deinit();
+
     while (end <= args.len) : (end += 1) {
         if (end == args.len or args[end] == ' ') {
             const isFlag: bool = std.mem.eql(u8, args[start .. start + 2], "--");
+            const isShortFlag: bool = args[start] == '-';
 
-            if (isFlag) start += 2;
+            if (isFlag) {
+                start += 2;
+            } else if (isShortFlag) {
+                start += 1;
+            }
 
             const slice = args[start..end];
 
@@ -48,6 +55,16 @@ pub fn readArgsIntoCommand(args: []const u8, allocator: std.mem.Allocator) !Comm
                 }
 
                 prevFlag = Flag{ .head = slice, .tail = null };
+            } else if (isShortFlag) {
+                // Allocate a slice for each value
+                var tempArray = [_]u8{slice};
+                const shortSlice: []u8 = tempArray[0..];
+
+                // Store the slice in the ArrayList
+                shortFlags.append(shortSlice) catch {
+                    std.debug.print("Failed to append\n", .{});
+                    return;
+                };
             } else {
                 if (prevFlag != null and prevFlag.?.tail == null) {
                     prevFlag.?.tail = slice;
@@ -60,14 +77,24 @@ pub fn readArgsIntoCommand(args: []const u8, allocator: std.mem.Allocator) !Comm
         }
     }
 
+    try printCommand(command);
+
     return command;
+}
+
+pub fn printCommand(command: Command) !void {
+    try stdout.print("command: {?s}\n\n", .{command.command});
+
+    for (command.flags.items, 0..) |flag, i| {
+        try stdout.print("flag: {d}\nh: {s}\nt: {?s}\n\n", .{ i, flag.head, flag.tail });
+    }
 }
 
 test "read args" {
     const args = try std.process.argsAlloc(test_allocator);
     defer std.process.argsFree(test_allocator, args);
 
-    try stdout.print("cli args: {s}\n", .{args});
+    //try stdout.print("cli args: {s}\n", .{args});
 }
 
 test "read slice manually" {
@@ -87,24 +114,30 @@ test "read slice manually" {
     }
 }
 
-pub fn printCommand(command: Command) !void {
-    try stdout.print("command: {?s}\n\n", .{command.command});
-
-    for (command.flags.items, 0..) |flag, i| {
-        try stdout.print("flag: {d}\nh: {s}\nt: {?s}\n\n", .{ i, flag.head, flag.tail });
-    }
-}
-
 test "read slice manually into flag object" {
     const args: []const u8 = "downloads --dry-run --type hard";
     const command: Command = try readArgsIntoCommand(args, test_allocator);
-    defer command.flags.deinit();
+    defer command.deinit();
 
-    try printCommand(command);
+    //try printCommand(command);
 
     //EXPECT
     try expect(std.mem.eql(u8, command.command.?, "downloads"));
     try expect(std.mem.eql(u8, command.flags.items[0].head, "dry-run"));
     try expect(std.mem.eql(u8, command.flags.items[1].head, "type"));
     try expect(std.mem.eql(u8, command.flags.items[1].tail.?, "hard"));
+}
+
+test "read shortflags into flag object" {
+    const args: []const u8 = "downloads -dhv";
+    const command: Command = try readArgsIntoCommand(args, test_allocator);
+    defer command.deinit();
+
+    try printCommand(command);
+
+    // EXPECT
+    try expect(std.mem.eql(u8, command.command.?, "downloads"));
+    try expect(std.mem.eql(u8, command.flags.items[0].head, "d"));
+    try expect(std.mem.eql(u8, command.flags.items[1].head, "h"));
+    try expect(std.mem.eql(u8, command.flags.items[2].head, "v"));
 }
