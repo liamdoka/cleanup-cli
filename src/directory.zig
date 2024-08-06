b1b4781be1c370430e04b2d93aef1767880c9062
Liam O'Kane <ldokane01@gmail.com>
2024-08-06 13:39:25 +1000

    use stdascii

--- src/directory.zig
+++ src/directory.zig
@@ -41,17 +41,144 @@ test "try read environment variables" {
 }
 
 test "find basic folders" {
+    const config: cleanup.Config = cleanup.Config{ .directory = "Downloads" };
+
     const homeVar = try std.process.getEnvVarOwned(test_allocator, "HOME");
     defer test_allocator.free(homeVar);
 
     const dir: std.fs.Dir = std.fs.cwd();
     const homeDir: std.fs.Dir = try dir.openDir(homeVar, .{ .iterate = true });
+    // std.debug.print("{any}\n", .{homeDir});
+    try homeDir.setAsCwd();
 
-    // var iter = try homeDir.iterate();
+    const homePath = try homeDir.realpathAlloc(test_allocator, ".");
+    defer test_allocator.free(homePath);
 
-    // while (iter.next()) |entry| {
-    //     std.debug.print("{s}\n", .{entry});
-    // }
+    try expect(eql(u8, homePath, homeVar));
 
-    std.debug.print("{any}\n", .{homeDir});
+    var homeIter: std.fs.Dir.Iterator = homeDir.iterate();
+    var found: bool = false;
+    while (try homeIter.next()) |result| {
+        switch (result.kind) {
+            std.fs.File.Kind.directory => {
+                // std.debug.print("dir: {s}\n", .{result.name});
+                if (eql(u8, result.name, config.directory)) {
+                    std.debug.print("MATCH :)\n", .{});
+                    found = true;
+                    break;
+                }
+            },
+            else => continue,
+        }
+    }
+
+    try expect(found);
+}
+
+test "case sensitivity" {
+    const config = cleanup.Config{
+        .directory = "downloads",
+    };
+
+    const foundDir = "DownLoaDs";
+    var buf: [foundDir.len]u8 = undefined;
+    const lowerDir = std.ascii.lowerString(&buf, foundDir);
+
+    try expect(eql(u8, lowerDir, config.directory));
+}
+
+test "case sensitivity in the wild" {
+    const config = cleanup.Config{ .directory = "deskTOP" };
+
+    var buf: [config.directory.len]u8 = undefined;
+    const configDirLower = std.ascii.lowerString(&buf, config.directory);
+
+    const homeVar = try std.process.getEnvVarOwned(test_allocator, "HOME");
+    defer test_allocator.free(homeVar);
+
+    const cwd = std.fs.cwd();
+    const homeDir = try cwd.openDir(homeVar, .{ .iterate = true });
+
+    var homeIter = homeDir.iterate();
+    var found = false;
+    while (try homeIter.next()) |result| {
+        if (result.kind == .directory) {
+            if (result.name.len != configDirLower.len) {
+                // cut it early if it wont be a match
+                continue;
+            }
+
+            var resultBuf: [config.directory.len]u8 = undefined;
+            const resultLower = std.ascii.lowerString(&resultBuf, result.name);
+
+            if (eql(u8, resultLower, configDirLower)) {
+                found = true;
+                break;
+            }
+        }
+    }
+
+    try expect(found);
+    try expect(eql(u8, configDirLower, "desktop"));
+}
+
+test "get all files and subdirectories in dir" {
+    const config = cleanup.Config{ .directory = "DownLoads" };
+
+    var buf: [config.directory.len]u8 = undefined;
+    const configDirLower = std.ascii.lowerString(&buf, config.directory);
+
+    const homeVar = try std.process.getEnvVarOwned(test_allocator, "HOME");
+    defer test_allocator.free(homeVar);
+
+    const cwd = std.fs.cwd();
+    const homeDir = try cwd.openDir(homeVar, .{ .iterate = true });
+
+    var homeIter = homeDir.iterate();
+    var found = false;
+    var configDirPath: []const u8 = undefined;
+    while (try homeIter.next()) |result| {
+        if (result.kind == .directory) {
+            if (result.name.len != configDirLower.len) {
+                // cut it early if it wont be a match
+                continue;
+            }
+
+            var resultBuf: [config.directory.len]u8 = undefined;
+            const resultLower = std.ascii.lowerString(&resultBuf, result.name);
+
+            if (eql(u8, resultLower, configDirLower)) {
+                found = true;
+                configDirPath = result.name;
+                break;
+            }
+        }
+    }
+
+    if (found) {
+        const configDir = try homeDir.openDir(configDirPath, .{ .iterate = true });
+        const testFileName: []const u8 = "testfile.txt";
+
+        const testFile: std.fs.File = try configDir.createFile(testFileName, .{});
+        testFile.close();
+
+        var outBuf: [1024]u8 = undefined;
+        const realpath = try configDir.realpath(".", &outBuf);
+        std.debug.print("{s}\n", .{realpath});
+
+        var dirIter = configDir.iterate();
+        var foundTestFile = false;
+        while (try dirIter.next()) |result| {
+            if (std.ascii.eqlIgnoreCase(result.name, testFileName)) {
+                foundTestFile = true;
+                break;
+            }
+        }
+
+        if (foundTestFile) {
+            try configDir.deleteFile(testFileName);
+        }
+
+        try expect(foundTestFile);
+    }
 }
